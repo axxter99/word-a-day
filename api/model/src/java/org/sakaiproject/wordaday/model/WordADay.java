6470c31d8384c78071995041fee4659d59412f05
David Horwitz <dhorwitz@gmail.com>
2007-05-06 18:27:41 +0000

    Initial import.

---
+++ api/model/src/java/org/sakaiproject/wordaday/model/WordADay.java
@@ -0,0 +1,49 @@
+package org.sakaiproject.wordaday.model;
+
+import java.util.Date;
+
+public class WordADay {
+	
+	private String word;
+	private String definition;
+	private Date updated;
+	
+	/*
+	 * 
+	 * constructors
+	 */
+	
+	public WordADay(String w, String def) {
+		this.word = w;
+		this.definition = def;
+		this.updated = new Date();
+		
+	}
+	
+	
+	public String getWord(){
+		return this.word;
+	}
+	
+	public void setWord(String word){
+		word = word;
+		
+	}
+	
+	public String getDefinition() {
+		return definition;
+	}
+	
+	public void setDefinition(String def) {
+		definition = def;
+	}
+	
+	public void setUpdated(Date date) {
+		updated = date;
+	}
+	
+	public Date getUpdated(){
+		return updated;
+	}
+
+}
---
+++ tool/src/java/org/sakaiproject/wordaday/tool/producers/WordADayProducer.java
@@ -0,0 +1,70 @@
+package org.sakaiproject.wordaday.tool.producers;
+
+import org.sakaiproject.news.api.NewsChannel;
+import org.sakaiproject.news.api.NewsItem;
+import org.sakaiproject.news.api.NewsService;
+import org.sakaiproject.wordaday.model.WordADay;
+
+import uk.org.ponder.rsf.components.UIContainer;
+import uk.org.ponder.rsf.components.UIOutput;
+import uk.org.ponder.rsf.components.UIVerbatim;
+import uk.org.ponder.rsf.view.ComponentChecker;
+import uk.org.ponder.rsf.view.DefaultView;
+import uk.org.ponder.rsf.view.ViewComponentProducer;
+import uk.org.ponder.rsf.viewstate.ViewParameters;
+
+import java.util.Date;
+import java.util.List;
+
+
+public class WordADayProducer implements DefaultView, ViewComponentProducer {
+
+	public static final String VIEW_ID = "wordaday";
+	public String getViewID() {
+		// TODO Auto-generated method stub
+		return VIEW_ID;
+	}
+
+	//inject the word
+	private WordADay word;
+	public void setWord(WordADay w) {
+		word = w;
+	}
+	
+	private NewsService newsService;
+	
+	public void setNewsService(NewsService ns) {
+		newsService = ns;
+		
+	}
+	//we need an init method
+	public void init() {
+		//
+		
+		
+	}
+	
+	public void fillComponents(UIContainer tofill, ViewParameters viewparams,
+			ComponentChecker checker) {
+		// TODO Auto-generated method stub
+		//for now just create the Word
+		//NewsChannel nc = newsService.getChannel("http://www.oed.com/rss.xml");
+		try {
+			Date check = new Date(System.currentTimeMillis() - 7200000);
+			if (word==null || word.getUpdated().before(check)) {
+				List items = newsService.getNewsitems("http://www.oed.com/rss.xml");
+				NewsItem ni = (NewsItem)items.get(0);
+				word = new WordADay(ni.getTitle(),ni.getDescription());
+			}
+		
+		
+		
+			UIOutput.make(tofill,"word",word.getWord());
+			UIVerbatim.make(tofill,"description", word.getDefinition());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
