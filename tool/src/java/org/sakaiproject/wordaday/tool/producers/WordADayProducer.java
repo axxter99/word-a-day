2080c7ae9ea69d4a44171fd7cbca4951bd62c315
David Horwitz <dhorwitz@gmail.com>
2015-07-28 21:00:05 +0200

    NOJIRA

--- tool/src/java/org/sakaiproject/wordaday/tool/producers/WordADayProducer.java
+++ tool/src/java/org/sakaiproject/wordaday/tool/producers/WordADayProducer.java
@@ -16,55 +16,52 @@ import uk.org.ponder.rsf.viewstate.ViewParameters;
 import java.util.Date;
 import java.util.List;
 
-
 public class WordADayProducer implements DefaultView, ViewComponentProducer {
 
 	public static final String VIEW_ID = "wordaday";
 	private WordADay word;
 	private NewsItem newsItem;
-	
+
 	public String getViewID() {
 		// TODO Auto-generated method stub
 		return VIEW_ID;
 	}
 
 	private String url;
-	public void setUrl(String u){
+
+	public void setUrl(String u) {
 		url = u;
 	}
-	
+
 	private NewsService newsService;
-	
+
 	public void setNewsService(NewsService ns) {
 		newsService = ns;
-		
+
 	}
-	//we need an init method
+
+	// we need an init method
 	public void init() {
 		//
-		
-		
+
 	}
-	
-	public void fillComponents(UIContainer tofill, ViewParameters viewparams,
-			ComponentChecker checker) {
+
+	public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker checker) {
 		// TODO Auto-generated method stub
-		//for now just create the Word
-		//NewsChannel nc = newsService.getChannel("http://www.oed.com/rss.xml");
+		// for now just create the Word
+		// NewsChannel nc =
+		// newsService.getChannel("http://www.oed.com/rss.xml");
 		try {
 			Date check = new Date(System.currentTimeMillis() - 7200000);
-			if (word==null || word.getUpdated().before(check)) {
+			if (word == null || word.getUpdated().before(check)) {
 				List items = newsService.getNewsitems(url);
-				NewsItem ni = (NewsItem)items.get(0);
-				word = new WordADay(ni.getTitle(),ni.getDescription());
+				NewsItem ni = (NewsItem) items.get(0);
+				word = new WordADay(ni.getTitle(), ni.getDescription());
 			}
-		
-		
-		
-			UIOutput.make(tofill,"word",word.getWord());
-			UIVerbatim.make(tofill,"description", word.getDefinition());
-		}
-		catch (Exception e) {
+
+			UIOutput.make(tofill, "word", word.getWord());
+			UIVerbatim.make(tofill, "description", word.getDefinition());
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
